a3567ff176d4eadb6a2b477d1c3410798335c250
ColinHammarberg <colin.hammarberg2@gmail.com>
2024-01-15 21:40:31 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlDev = "https://gentle-smoke-0d09bd810.4.azurestaticapps.net/";
+const urlProd = "https://gentle-smoke-0d09bd810.4.azurestaticapps.net/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
